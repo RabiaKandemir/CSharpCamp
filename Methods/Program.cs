d870b3f0684da9587dfb532c097fea0cba5144d6
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2023-11-08 20:49:59 +0300

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product()
+            {
+                Id = 1,
+                Name = "Elma",
+                Description = "Amasya elması",
+                Price = 15,
+            };
+            Product product2 = new Product()
+            {
+                Id = 2,
+                Name = "Karpuz",
+                Description = "Diyarbakır karpuzu",
+                Price = 80,
+            };
+            Product[] products= new Product[] { product1, product2 };
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.Description);
+                Console.WriteLine("********************");
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------Metotlar--------------");
+            BasketManager basketManager = new BasketManager();
+            basketManager.Add(product1);
+            basketManager.Add(product2);
+
+        }
+    }
+}
