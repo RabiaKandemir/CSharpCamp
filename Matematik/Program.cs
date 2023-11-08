aded9db1d8f881f8ab4f0cf69266b3e7cc5b7644
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2023-11-08 22:20:25 +0300

    Metotlar

---
+++ Matematik/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Matematik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FourOperations fourOperations = new FourOperations();
+            fourOperations.Sum(5, 7);
+        }
+    }
+}
--- Methods/Product.cs
+++ Methods/Product.cs
@@ -13,5 +13,6 @@ namespace Methods
         public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
+        public int StockNumber { get; set; }
     }
 }
