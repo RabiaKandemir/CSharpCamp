26e17dd03e55b02cb58e096b28d2eff2899ff8a9
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2023-11-30 00:42:33 +0300

    my dictionary

---
+++ Dictionary/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public class MyList<T>
+    {
+        private T[] values;
+        public MyList()
+        {
+            values = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = values;
+            values =new T[values.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                values[i] = tempArray[i];
+            }
+            values[values.Length-1] = item;
+        }
+     public int Count {  get { return values.Length; } }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            Console.WriteLine(myList.Count);
+            myList.Add(2);
+            Console.WriteLine(myList.Count);
+        }
+    }
+}
